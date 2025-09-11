277f3a7fca2f69cd9e652688d75643a511898ff8
Ritik-1118 <ritik11181@gmail.com>
2025-09-11 22:13:59 +0530

    Update socket connection URL to use production endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL = 'http://localhost:8000'
+// const URL = 'http://localhost:8000'
+const URL = 'https://sketchboard-p4sh.onrender.com'
 export const socket = io(URL);
